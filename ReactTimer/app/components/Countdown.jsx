82ed1abb6a526fd294cff20ea7153424c01d28e6
KanesYang <978634806@qq.com>
2017-05-20 18:04:05 -0400

    set up of the timer app

---
+++ ReactTimer/app/components/Countdown.jsx
@@ -0,0 +1,49 @@
+var React = require('react');
+var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
+
+var Countdown = React.createClass({
+    getInitialState: function () {
+        return {
+            count: 0,
+            CountdownStatus: 'stopped'
+        };
+    },
+    componentDidUpdate: function(prevProps, prevState) {
+        if (this.state.CountdownStatus !== prevState.CountdownStatus) {
+            switch(this.state.CountdownStatus) {
+                case 'started':
+                    this.startTimer();
+                    break;
+                case 'stopped':
+
+                    break;
+            }
+        }
+    },
+    startTimer: function() {
+        this.timer = setInterval(() => {
+            var newCount = this.state.count - 1;
+            this.setState({
+                count: newCount >= 0 ? newCount : 0
+            });
+        }, 1000)
+    },
+    handleSetCountdown: function(seconds) {
+        this.setState({
+            count: seconds,
+            CountdownStatus: 'started'
+        });
+    },
+    render: function () {
+        var {count} = this.state;
+        return (
+            <div>
+                <Clock totalSeconds={count} />
+                <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+            </div>
+        );
+    }
+});
+
+module.exports = Countdown;
