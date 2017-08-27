f4c542738ecb959154f99deccfcd98454cdab6d8
KanesYang <978634806@qq.com>
2017-08-27 01:41:16 -0400

    json server app artist

---
+++ revPro/musicdb/src/components/home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import Banner from './banner';
+import ArtistsList from './artistsList';
+
+const URL_ARTISTS = 'http://localhost:3004/artists';
+
+export default class Home extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            artists: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL_ARTISTS, {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                artists: json
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Banner /> 
+                <ArtistsList allArtists={this.state.artists}/> 
+            </div>
+        );
+    }
+}
